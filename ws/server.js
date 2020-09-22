5e9b113443bdc0b2ce17e2cd0d0326fa26a84f23
Filip Dujmusic <filip@ampnet.io>
2020-09-22 12:13:34 +0200

    Cleanup websocket connections after connection close event.

--- ws/server.js
+++ ws/server.js
@@ -8,6 +8,7 @@ let config = require('../config')
 
 let ws
 let subscriptions = {}
+let connections = {}
 
 async function start(server) {
     let port = config.get().ws.port 
@@ -20,6 +21,7 @@ async function start(server) {
 
     ws.on('connection', (socket) => {
         socket.id = uuid()
+        logger.info(`WS: Connection opened. ID: ${socket.id}`)
         socket.on('message', (data) => {
             let json
             
@@ -43,10 +45,29 @@ async function start(server) {
             }
 
             logger.info(`WS: Subscribing to transaction updates for wallet ${wallet}`)
+            addNewConnection(socket, wallet)
             addNewSubscriber(socket, wallet)
         })
-
+        socket.on('close', (e) => {
+            let wallets = connections[socket.id] || []
+            logger.info(`WS: Closing connection ${socket.id}. Connection tracks wallets: ${wallets}`)
+            for (wallet of wallets) {
+                let subs = subscriptions[wallet] || []
+                subscriptions[wallet] = subs.filter(c => c.id !== socket.id)
+                logger.info(`WS: Removing subscription for wallet ${wallet} and connection ${socket.id}`)
+            }
+            connections[socket.id] = undefined
+            logger.info(`WS: Removed connection ${socket.id}`)
+        })
     })
+}
+
+function addNewConnection(connection, wallet) {
+    connections[connection.id] = connections[connection.id] || []
+    if (!connections[connection.id].includes(wallet)) {
+        connections[connection.id].push(wallet)
+        logger.info(`WS: Added new connection with id: ${connection.id}. Connection tracks wallet ${wallet}`)
+    }
 }
 
 function addNewSubscriber(connection, wallet) {
